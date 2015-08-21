25c11863aefc14e718c90f2902974c39e6253c9e
Ondra Tethal <ondrej.tethal@qoretechnologies.com>
2015-08-21 11:25:52 +0200

    Simple expression parser

--- tools/driver/driver.cpp
+++ tools/driver/driver.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include "qore/common/common.h"
+#include "parser.hpp"
+
+void yyerror(const char *msg) {
+    printf("Error: %s\n", msg);
+}
 
 int main() {
     std::cout << "test(6) = " << test(6) << std::endl;
+    yyparse();
     return 0;
 }
-
