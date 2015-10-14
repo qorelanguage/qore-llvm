c662210de1ddba41b809fa3f34a3cbd7f274ac1a
Ondra Tethal <ondrej.tethal@qoretechnologies.com>
2015-10-14 22:52:08 +0200

    Lookup and filtering of qtif files

---
+++ test/Qtif.h
@@ -0,0 +1,41 @@
+//--------------------------------------------------------------------*- C++ -*-
+//
+//  Qore Programming Language
+//
+//  Copyright (C) 2015 Qore Technologies
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//  DEALINGS IN THE SOFTWARE.
+//
+//------------------------------------------------------------------------------
+#ifndef TEST_QTIF_H_
+#define TEST_QTIF_H_
+
+namespace qtif {
+
+inline std::vector<std::string> findFiles(const std::string &filter) {
+    std::vector<std::string> files;
+    #define QTIF(n)     if (contains(n, filter)) { files.push_back(n); }
+    #include "test_files.inc"
+    #undef QTIF
+    return files;
+}
+
+} // namespace qtif
+
+#endif // TEST_QTIF_H_
---
+++ test/qtif/QtifTest.cpp
@@ -0,0 +1,41 @@
+//--------------------------------------------------------------------*- C++ -*-
+//
+//  Qore Programming Language
+//
+//  Copyright (C) 2015 Qore Technologies
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//  DEALINGS IN THE SOFTWARE.
+//
+//------------------------------------------------------------------------------
+#include "gmock/gmock.h"
+#include "../Qtif.h"
+
+#define QTIF(a)
+#include "test_files.inc"
+#undef QTIF
+
+namespace qtif {
+
+TEST(Qtif, findFiles) {
+    std::vector<std::string> files = findFiles("qtif/");
+    EXPECT_EQ(1U, files.size());
+    EXPECT_EQ(std::string(TEST_INPUT_DIR) + "/qtif/test1.qtif", files[0]);
+}
+
+} // namespace qtif
